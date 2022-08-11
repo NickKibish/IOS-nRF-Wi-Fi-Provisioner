1610f6aced92914133e42ef4382e45e673d5f9e2
Nick Kibysh <nick.kibysh@gmail.com>
2022-08-11 11:53:03 +0200

    feat(Fastlane): Initial
    Created initial fastlane config with incremented build, build app, submit to testflight

---
+++ fastlane/Fastfile.swift
@@ -0,0 +1,19 @@
+// This file contains the fastlane.tools configuration
+// You can find the documentation at https://docs.fastlane.tools
+//
+// For a list of all available actions, check out
+//
+//     https://docs.fastlane.tools/actions
+//
+
+import Foundation
+
+class Fastfile: LaneFile {
+    func betaLane() {
+        desc("Increment build number, build app and deploy to TestFlight")
+        
+        incrementBuildNumber()
+        buildIosApp(xcodebuildFormatter: "xcpretty")
+        uploadToTestflight()
+    }
+}
