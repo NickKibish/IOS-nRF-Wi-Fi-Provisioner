d4222c912f4422cbabcc9ed1f6d495cd5ae790fd
Nick Kibysh <nick.kibysh@gmail.com>
2022-08-12 11:31:59 +0200

    restored full beta lane

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -17,15 +17,15 @@ class Fastfile: LaneFile {
         desc("Increment build number, build app and deploy to TestFlight")
         
         incrementBuildNumber()
-//        buildIosApp(xcodebuildFormatter: "xcpretty")
-//        
-//        let keyId = environmentVariable(get: "ASC_KEY_ID")
-//        let issuerId = environmentVariable(get: "ASC_ISSUER_ID")
-//        let keyContent = environmentVariable(get: "ASC_PRIVATE_KEY")
-//        
-//        desc("loaded keys: \(keyId), issuer: \(issuerId), keyContent: \(keyContent)")
-//        
-//        appStoreConnectApiKey(keyId: keyId, issuerId: issuerId, keyContent: OptionalConfigValue(stringLiteral: keyContent), isKeyContentBase64: true)
-//        uploadToTestflight()
+        buildIosApp(xcodebuildFormatter: "xcpretty")
+
+        let keyId = environmentVariable(get: "ASC_KEY_ID")
+        let issuerId = environmentVariable(get: "ASC_ISSUER_ID")
+        let keyContent = environmentVariable(get: "ASC_PRIVATE_KEY")
+
+        desc("loaded keys: \(keyId), issuer: \(issuerId), keyContent: \(keyContent)")
+
+        appStoreConnectApiKey(keyId: keyId, issuerId: issuerId, keyContent: OptionalConfigValue(stringLiteral: keyContent), isKeyContentBase64: true)
+        uploadToTestflight()
     }
 }
